faf277eebe0a740bbaab6d966e911bbc853b3314
James Borders <James.W.Borders@gmail.com>
2014-04-26 18:38:24 -0700

    minor tweaks to dialog

--- src/param_dialog.c
+++ src/param_dialog.c
@@ -32,7 +32,7 @@ static int parse_value(const char *str, param_type_t type, void *param_val)
         errno = 0;
         char *endptr;
         double d = strtod(str, &endptr);
-        if(errno || (endptr == str) || (*endptr != NULL))
+        if(errno || (endptr == str) || (*endptr != '\0'))
         {
           printf("error parsing float from string: %s\n", str);
           return -1;
@@ -46,7 +46,7 @@ static int parse_value(const char *str, param_type_t type, void *param_val)
         errno = 0;
         char *endptr;
         long int l = strtol(str, &endptr, 0);
-        if(errno || (endptr == str) || (*endptr != NULL))
+        if(errno || (endptr == str) || (*endptr != '\0'))
         {
           printf("error parsing int from string: %s\n", str);
           return -1;
@@ -72,7 +72,6 @@ int param_dialog(char *param_name, param_type_t type, void *param_val)
   char title[255];
   int ret;
 
-  //snprintf(title, sizeof(title), "Enter value for \"%s\"...", param_name);
   snprintf(title, sizeof(title), "%s=...", param_name);
 
   dialog = gtk_dialog_new_with_buttons (title, NULL,
@@ -83,25 +82,34 @@ int param_dialog(char *param_name, param_type_t type, void *param_val)
 
   content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
   entry = gtk_entry_new();
-  gtk_entry_set_text((GtkEntry *)entry, 
-      value_to_string(param_val, type));
 
   gtk_container_add (GTK_CONTAINER (content_area), entry);
 
-   gtk_widget_show_all(dialog);
+  gtk_widget_show_all(dialog);
 
-  gint result = gtk_dialog_run(GTK_DIALOG (dialog));
-  switch (result)
+  int done = 0;
+  while(!done)
   {
-    case GTK_RESPONSE_ACCEPT:
-      ret = parse_value(gtk_entry_get_text((GtkEntry *)entry), type, param_val);
-      break;
-    case GTK_RESPONSE_REJECT:
-      printf("cancelled\n");
-      break;
-    default:
-      printf("unknown response code: %d\n", result);
-      break;
+    gtk_entry_set_text((GtkEntry *)entry, 
+        value_to_string(param_val, type));
+    gtk_widget_grab_focus(entry);
+    gint result = gtk_dialog_run(GTK_DIALOG (dialog));
+    switch (result)
+    {
+      case GTK_RESPONSE_ACCEPT:
+        ret = parse_value(gtk_entry_get_text((GtkEntry *)entry), type, param_val);
+        if(ret == 0)
+          done = 1;
+        break;
+      case GTK_RESPONSE_REJECT:
+        printf("cancelled\n");
+        done = 1;
+        break;
+      default:
+        printf("unknown response code: %d\n", result);
+        done = 1;
+        break;
+    }
   }
   gtk_widget_destroy(dialog);
 
